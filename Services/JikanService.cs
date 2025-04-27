1283ab197aa610711c8da9697e7837e415bdd201
noodle <nonomi2603@gmail.com>
2025-04-27 15:56:12 +0600

    #1 Create service

---
+++ Services/JikanService.cs
@@ -0,0 +1,20 @@
+﻿namespace aniwiki_backend.Services;
+
+public class JikanService
+{
+    private readonly HttpClient _httpClient;
+
+    public JikanService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri("https://api.jikan.moe/v4/");
+    }
+
+    public async Task<string> SearchAnimeAsync(string query)
+    {
+        var response = await _httpClient.GetAsync($"anime?q={query}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        return content;
+    }
+}
