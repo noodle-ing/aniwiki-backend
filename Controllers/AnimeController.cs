0609870169a64eea9caca940e7182747f03a1406
noodle <nonomi2603@gmail.com>
2025-04-27 16:11:31 +0600

    #2 Add Controller

---
+++ Controllers/AnimeController.cs
@@ -0,0 +1,21 @@
+﻿using aniwiki_backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aniwiki_backend.Controllers;
+
+public class AnimeController : ControllerBase
+{
+    private readonly JikanService _jikanService;
+
+    public AnimeController(JikanService jikanService)
+    {
+        _jikanService = jikanService;
+    }
+
+    [HttpGet("search")]
+    public async Task<IActionResult> SerachAnimeAsync(string query)
+    {
+        var result = await _jikanService.SearchAnimeAsync(query);
+        return Content(result, "application/json");
+    }
+}
